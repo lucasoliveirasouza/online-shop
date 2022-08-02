86bd75279e14029962459c8ef60daaa01cd2c1db
lucassouzapb <lycasoliveira@gmail.com>
2022-08-02 17:17:11 -0300

    Criar Home

--- lib/views/home/components/categories.dart
+++ lib/views/home/components/categories.dart
@@ -51,7 +51,7 @@ class _CategoriesState extends State<Categories> {
               ),
               Container(
                 margin: EdgeInsets.only(top: kDefaultPaddin / 4),
-                height: 2,,
+                height: 2,
                 width: 30,
                 color:
                     selectedIndex == index ? Colors.black : Colors.transparent,
