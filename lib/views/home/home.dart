c71aa614ddc33c4964a2bc0ecd1f71bba3d4ce52
lucassouzapb <lycasoliveira@gmail.com>
2022-08-02 16:40:19 -0300

    Criar Home

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -2,39 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:onlineshop/util/constants.dart';
 
+import 'components/body.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: SvgPicture.asset("assets/icons/back.svg"),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              "assets/icons/search.svg",
-              color: kTextColor,
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              "assets/icons/cart.svg",
-              color: kTextColor,
-            ),
-          ),
-          SizedBox(
-            width: kDefaultPaddin / 2,
-          ),
-        ],
+      appBar: appBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+        onPressed: () {},
       ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            color: kTextColor,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            color: kTextColor,
+          ),
+        ),
+        SizedBox(
+          width: kDefaultPaddin / 2,
+        ),
+      ],
     );
   }
 }
