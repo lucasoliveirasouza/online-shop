fe6d97d6dadba79b16e75118dc14b99550617732
lucassouzapb <lycasoliveira@gmail.com>
2022-08-02 16:30:41 -0300

    Criar Home

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/back.svg"),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/search.svg"),
+          ),
+        ],
+      ),
+    );
   }
 }
