99bea0470324911601ecf39fdfc197735f657e95
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-04 19:46:59 -0300

    Atualizando home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:onlineshop/util/constants.dart';
 import 'views/home/home.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Online Shop',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
