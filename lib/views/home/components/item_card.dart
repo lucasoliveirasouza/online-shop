bf7ac33b235a884c46992a9b169f3f4fc5680a20
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-04 19:51:44 -0300

    Atualizando home

--- lib/views/home/components/item_card.dart
+++ lib/views/home/components/item_card.dart
@@ -4,41 +4,44 @@ import 'package:onlineshop/util/constants.dart';
 
 class ItemCard extends StatelessWidget {
   final Product product;
-  final Function press;
+  final VoidCallback press;
   ItemCard({Key? key, required this.product, required this.press}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Container(
-            padding: EdgeInsets.all(kDefaultPaddin),
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPaddin),
 
-            decoration: BoxDecoration(
-              color: product.color,
-              borderRadius: BorderRadius.circular(16),
+              decoration: BoxDecoration(
+                color: product.color,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Image.asset(product.image),
             ),
-            child: Image.asset(product.image),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin/4),
-          child: Text(
-            product.title,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin/4),
+            child: Text(
+              product.title,
+              style: TextStyle(
+                color: kTextLightColor,
+              ),
+            ),
+          ),
+          Text(
+            "\$ ${product.price}",
             style: TextStyle(
-              color: kTextLightColor,
+              fontWeight: FontWeight.bold,
             ),
-          ),
-        ),
-        Text(
-          "\$ ${product.price}",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
