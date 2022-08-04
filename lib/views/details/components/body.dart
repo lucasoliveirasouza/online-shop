a367e5137d5ef56ba08dd1d4e1d862913ba68d88
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-04 20:06:35 -0300

    Atualizando home

--- lib/views/details/components/body.dart
+++ lib/views/details/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop/models/Product.dart';
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+  Product product;
+  Body({Key? key, required this.product}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
