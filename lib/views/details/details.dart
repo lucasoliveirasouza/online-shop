5e7a574dccded8a16e5f3767b7f296f7c89e2e1a
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-04 19:53:01 -0300

    Atualizando home

--- lib/views/details/details.dart
+++ lib/views/details/details.dart
@@ -7,6 +7,8 @@ class DetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: product.color,
+    );
   }
 }
