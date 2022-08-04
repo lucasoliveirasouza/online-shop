a55d24c954d36ffa43bda26a49fe35f76e0ccf70
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-04 19:43:00 -0300

    Atualizando home

---
+++ lib/views/details/details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:onlineshop/models/Product.dart';
+
+class DetailsView extends StatelessWidget {
+   Product product;
+   DetailsView({ Key? key , required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
