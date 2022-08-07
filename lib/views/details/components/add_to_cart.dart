0041b1af44b9eace6ee86dffdb3b13e64abf4f11
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-07 20:11:59 -0300

    Atualizando detalhes

--- lib/views/details/components/add_to_cart.dart
+++ lib/views/details/components/add_to_cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:onlineshop/models/Product.dart';
 import 'package:onlineshop/util/constants.dart';
 
@@ -11,7 +12,26 @@ class AddToCart extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: kDefaultPaddin),
       child: Row(
-        children: <Widget>[],
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: kDefaultPaddin),
+            height: 50,
+            width: 58,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              border: Border.all(
+                color: product.color,
+              ),
+            ),
+            child: IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/add_to_cart.svg",
+                color: product.color,
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
