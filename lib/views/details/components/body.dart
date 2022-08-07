8a6f7bc21892e7e5777df242dc64745b5976e7e7
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-07 18:48:14 -0300

    Atualizando detalhes

--- lib/views/details/components/body.dart
+++ lib/views/details/components/body.dart
@@ -39,27 +39,6 @@ class Body extends StatelessWidget {
                           Column(
                             children: [
                               Text("Color"),
-                              Container(
-                                margin: EdgeInsets.only(
-                                  top: kDefaultPaddin / 4,
-                                  right: kDefaultPaddin / 2,
-                                ),
-                                padding: EdgeInsets.all(2.5),
-                                height: 24,
-                                width: 24,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    color: Color(0xFF356C95),
-                                  ),
-                                ),
-                                child: DecoratedBox(
-                                  decoration: BoxDecoration(
-                                    color: Color(0xFF356C95),
-                                    shape: BoxShape.circle,
-                                  ),
-                                ),
-                              ),
                             ],
                           )
                         ],
--- lib/views/details/components/color_dot.dart
+++ lib/views/details/components/color_dot.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop/util/constants.dart';
 
 class ColorDot extends StatelessWidget {
-  const ColorDot({Key? key}) : super(key: key);
+  final Color color;
+  final bool isSelect;
+  ColorDot({
+    Key? key,
+    required this.color,
+    this.isSelect = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.only(
+        top: kDefaultPaddin / 4,
+        right: kDefaultPaddin / 2,
+      ),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Color(0xFF356C95),
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
   }
 }
