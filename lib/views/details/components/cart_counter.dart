4e69defbd9f23f72548c37476e6abde27d315b1a
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-08-07 19:12:43 -0300

    Atualizando detalhes

---
+++ lib/views/details/components/cart_counter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class CartCounter extends StatelessWidget {
+  const CartCounter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/views/details/components/counter_with_fav_btn.dart
+++ lib/views/details/components/counter_with_fav_btn.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:onlineshop/views/details/components/cart_counter.dart';
 
 class CounterWithFavBtn extends StatelessWidget {
   const CounterWithFavBtn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        CartCounter(),
+        Container(
+          padding: EdgeInsets.all(8),
+          height: 32,
+          width: 32,
+          decoration: BoxDecoration(
+            color: Color(0xFFFF6464),
+            shape: BoxShape.circle,
+          ),
+          child: SvgPicture.asset("assets/icons/heart.svg"),
+        )
+      ],
+    );
   }
 }
